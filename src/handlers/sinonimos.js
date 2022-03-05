826fec2942a387b15b4e490bd2e836925d8201b5
ThiagoNelsi <thiagonelsi@gmail.com>
2022-03-05 19:20:20 -0300

    Implements synonyms option

---
+++ src/handlers/sinonimos.js
@@ -0,0 +1,18 @@
+const { default: axios } = require('axios');
+const { wordStyle, cyan, red } = require('../utils/chalkUtils');
+
+async function sinonimos(word) {
+  try {
+    const { data } = await axios.get(`https://significado.herokuapp.com/synonyms/${word}`);
+
+    console.log(`\tSinônimos de ${wordStyle(word.toUpperCase())}:\n`);
+
+    data.forEach((synonym) => {
+      console.log(`\t${cyan('*')} ${synonym}`);
+    });
+  } catch (error) {
+    console.log(red(`\tSinônimos de ${word} não encontrado.`));
+  }
+}
+
+module.exports = sinonimos;
